3247dbe5d3646f85105b949c4e81d8ee40b0de41
Serhat Ece <134696090+serhatecee@users.noreply.github.com>
2024-07-24 14:57:24 +0300

    Add exercise files

--- Practice/03_12/Backpack.js
+++ Practice/03_12/Backpack.js
@@ -5,9 +5,10 @@
  * Class expression:  const Name = class {}
  */
 
+// Backpack sınıfı tanımlama
 class Backpack {
   constructor(
-    // Defines parameters:
+    // Parametreleri tanımlama:
     name,
     volume,
     color,
@@ -16,7 +17,7 @@ class Backpack {
     strapLengthR,
     lidOpen
   ) {
-    // Define properties:
+    // Özellikleri tanımlama:
     this.name = name;
     this.volume = volume;
     this.color = color;
@@ -27,14 +28,57 @@ class Backpack {
     };
     this.lidOpen = lidOpen;
   }
-  // Add methods like normal functions:
+
+  // Metodları normal fonksiyonlar gibi ekleme:
   toggleLid(lidStatus) {
     this.lidOpen = lidStatus;
   }
+
   newStrapLength(lengthLeft, lengthRight) {
     this.strapLength.left = lengthLeft;
     this.strapLength.right = lengthRight;
   }
+
+  // Yeni hacim belirleme metodu
+  changeVolume(newVolume) {
+    this.volume = newVolume;
+  }
+
+  // Yeni renk belirleme metodu
+  changeColor(newColor) {
+    this.color = newColor;
+  }
+
+  // Yeni cep sayısı belirleme metodu
+  changePocketNum(newPocketNum) {
+    this.pocketNum = newPocketNum;
+  }
 }
 
+// Sınıfı dışa aktarma
 export default Backpack;
+
+// Yeni bir Backpack nesnesi oluşturalım ve metodları test edelim:
+const myBackpack = new Backpack("Günlük Çanta", 30, "gri", 15, 26, 26, false);
+
+console.log("Başlangıç durumu:", myBackpack);
+
+// Kapak durumunu değiştir
+myBackpack.toggleLid(true);
+console.log("Kapak durumu değiştirildi:", myBackpack.lidOpen);
+
+// Askı uzunluğunu değiştir
+myBackpack.newStrapLength(30, 30);
+console.log("Askı uzunluğu değiştirildi:", myBackpack.strapLength);
+
+// Hacmi değiştir
+myBackpack.changeVolume(40);
+console.log("Hacim değiştirildi:", myBackpack.volume);
+
+// Rengi değiştir
+myBackpack.changeColor("mavi");
+console.log("Renk değiştirildi:", myBackpack.color);
+
+// Cep sayısını değiştir
+myBackpack.changePocketNum(20);
+console.log("Cep sayısı değiştirildi:", myBackpack.pocketNum);
--- Practice/03_12/script.js
+++ Practice/03_12/script.js
@@ -6,3 +6,58 @@
  * - Create several objects using the class.
  * - Test the objecs by calling their properties and using their methods in the console.
  */
+
+// Ayakkabı sınıfını tanımlama
+class Shoe {
+  constructor(
+    // Parametreleri tanımlama:
+    brand,
+    size,
+    color,
+    type,
+    isWorn
+  ) {
+    // Özellikleri tanımlama:
+    this.brand = brand;
+    this.size = size;
+    this.color = color;
+    this.type = type; // Spor ayakkabı, günlük ayakkabı, vs.
+    this.isWorn = isWorn; // Giyilmiş mi giyilmemiş mi
+  }
+
+  // Metodları normal fonksiyonlar gibi ekleme:
+  wear() {
+    this.isWorn = true;
+    console.log(`${this.brand} ayakkabı giyildi.`);
+  }
+
+  clean() {
+    this.isWorn = false;
+    console.log(`${this.brand} ayakkabı temizlendi.`);
+  }
+
+  changeColor(newColor) {
+    this.color = newColor;
+    console.log(
+      `${this.brand} ayakkabının rengi ${newColor} olarak değiştirildi.`
+    );
+  }
+}
+
+// Sınıftan nesneler oluşturma
+const sportShoe = new Shoe("Nike", 42, "siyah", "Spor", false);
+const casualShoe = new Shoe("Adidas", 41, "beyaz", "Günlük", true);
+const formalShoe = new Shoe("Gucci", 43, "kahverengi", "Klasik", false);
+
+// Nesneleri ve metodları test edelim:
+console.log("Başlangıç durumu:", sportShoe);
+sportShoe.wear();
+console.log("Giyildikten sonra:", sportShoe);
+
+console.log("Başlangıç durumu:", casualShoe);
+casualShoe.clean();
+console.log("Temizlendikten sonra:", casualShoe);
+
+console.log("Başlangıç durumu:", formalShoe);
+formalShoe.changeColor("siyah");
+console.log("Renk değiştirildikten sonra:", formalShoe);
